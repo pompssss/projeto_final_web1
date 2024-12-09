304dff80aa46dfcfcb69c504e18100e2a2d392ef
Rafael Pomponio <rafaelluispomponio@gmail.com>
2024-12-09 20:45:38 -0300

    subindo projeto

---
+++ server/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Gigicnh!0209",
+  database: "crudgames",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let mysql = "INSERT INTO crudgames.games ( name, cost, category) VALUES (?, ?, ?)";
+  db.query(mysql, [ name, cost, category], (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/search", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let mysql =
+    "SELECT * from crudgames.games WHERE name = ? AND cost = ? AND category = ?";
+  db.query(mysql, [name, cost, category], (err, result) => {
+    if (err) res.send(err);
+    res.send(result);
+  });
+});
+
+app.get("/getCards", (req, res) => {
+  let mysql = "SELECT * FROM crudgames.games";
+  db.query(mysql, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/edit", (req, res) => {
+  const { id } = req.body;
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+  let mysql = "UPDATE crudgames.games SET name = ?, cost = ?, category = ? WHERE id = ?";
+  db.query(mysql, [name, cost, category, id], (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  let mysql = "DELETE FROM crudgames.games WHERE id = ?";
+  db.query(mysql, id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("rodando na porta 3000");
+});
